45bbe172618ac3a0b4d52e2ced5e4587f78c45a9
Thomas <zawlintun@robust.best>
2024-05-13 10:24:40 +0700

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import '../css/app.css'
+
+import { createApp } from 'vue';
+
+import App from './components/app.vue'
+
+const app = createApp(App)
+
+app.mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['resources/js/**/*.{vue,js}', 'resources/views/**/*.blade.php'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
