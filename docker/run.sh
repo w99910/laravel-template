c586b3e0acff71ec53862e240cc721f6e8ee09f2
Thomas <zawlintun@robust.best>
2024-07-15 21:39:18 +0700

    update

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# in order to use npm and node, load $NVM_SH which is /home/${USER}/.nvm/nvm.sh since bashrc not working because the shell is not interactive
+. $NVM_SH
+
 cd /var/www/html/ || exit
 
 composer install --no-dev --no-interaction --no-ansi --audit
@@ -13,6 +16,15 @@ initialStuff() {
     php artisan config:cache; \
     php artisan route:cache; \
     php artisan storage:link
+
+    if [ -f ./package-lock.json ]; \
+      then \
+        npm ci --loglevel=error --no-audit; \
+      else \
+        npm install --loglevel=error --no-audit; \
+    fi
+
+    npm run build
 }
 
 initialStuff
